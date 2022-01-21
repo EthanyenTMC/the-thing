25c8e92f7da02d28d7fff73c8849234c305e8e0a
EthanyenTMC <68873703+EthanyenTMC@users.noreply.github.com>
2022-01-20 22:33:21 -0500

    asdfa

--- the thing/Assets/Scripts/playerController.cs
+++ the thing/Assets/Scripts/playerController.cs
@@ -11,7 +11,7 @@ public class playerController : MonoBehaviour
     private Vector3 movement;
     [SerializeField] private float walkSpeed, sprintSpeed, speed;
     private bool grounded;
-    private Vector3 playerVelocity, direction;
+    private Vector3 playerVelocity, direction, vertical;
 
     [SerializeField] private Transform camera;
     private float turnSmoothTime = 0.1f;
@@ -28,26 +28,43 @@ public class playerController : MonoBehaviour
     {
         if(!controller.isGrounded){
             Debug.Log("graviting");
-            controller.Move(new Vector3(0,worldController.getGravity(),0));
+            vertical += new Vector3(0, worldController.getGravity(),0);
+            //controller.Move(new Vector3(0,worldController.getGravity(),0));
         }
-        playerVelocity.x = Input.GetAxis("Horizontal");
-        playerVelocity.z = Input.GetAxis("Vertical");  
+        playerVelocity.x = Input.GetAxis("Horizontal") * speed;
+        playerVelocity.z = Input.GetAxis("Vertical") * speed;  
 
         if(Input.GetKey(KeyCode.LeftShift)){
             speed = sprintSpeed;
         }else{
             speed = walkSpeed;
         }
+        if(Input.GetKey(KeyCode.Space) && controller.isGrounded){
+            //playerVelocity.y += 0.1f;
+            vertical += new Vector3(0, 1,0);
+        }
 
-        direction = playerVelocity.normalized;
+
+        direction = new Vector3(playerVelocity.x, 0 ,playerVelocity.z).normalized;
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        
+
+        movement = Quaternion.Euler(0f,targetAngle,0f) * Vector3.forward;
         if(direction.magnitude >= 0.1f){
+            controller.transform.rotation = Quaternion.Euler(0f,targetAngle,0f);
+            controller.Move(movement*speed*Time.deltaTime + vertical);
+        }
+        
+        /*if(direction.magnitude >= 0.1f){
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             controller.transform.rotation = Quaternion.Euler(0f,targetAngle,0f);
 
             movement = Quaternion.Euler(0f,targetAngle,0f) * Vector3.forward;
             controller.Move(movement*speed*Time.deltaTime);
-        }
+        }*/
+
         
 
     }
